6ae4f34980a4ca36a43df35dc1db9ae8d460d832
SURAJ BHUSARE <146182813+Bhusaresuraj@users.noreply.github.com>
2024-12-08 16:48:37 +0530

    Add new files

---
+++ loop1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	int n;
+	cout<<"enter the numbers to be printed"<<endl;
+	cin>>n;
+	for(int i=0;i<=n;i++)
+	{
+		cout<<"The number to be printed as:"<<i<<endl;
+	}
+	return 0;
+}
---
+++ ternary.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number;
+cout<<"Enter the number to get the value"<<endl;
+cin>>number;
+cout<<(number>0 ? "positive":"negetive")<<endl;
+}
+
+//ternary(either the value is positive of negetive)
