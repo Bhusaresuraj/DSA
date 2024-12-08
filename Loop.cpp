5925e1f4242d3bc5001c1c8f34a40abdd59c3506
SURAJ BHUSARE <146182813+Bhusaresuraj@users.noreply.github.com>
2024-12-08 13:59:32 +0530

    Add new files

---
+++ Loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n=-66;
+  
+  if(n>=0)
+  {
+    cout<<" the number is greater than zero"<<endl;
+  }
+  else 
+  {
+     cout<<"the number not greater than zero"<<endl;
+  }
+}
